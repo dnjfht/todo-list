d7d5854763954a1c2d71e75a0e05cfcf47986850
Yoo Seungmin <dnjfht@gmail.com>
2023-05-16 12:52:14 +0900

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styles from "./App.module.css";
+import { v4 as uuidv4 } from "uuid";
+import { CiLight } from "react-icons/ci";
 
 function App() {
+  const [todo, setTodo] = useState(initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.Wrap}>
+      {/* todo container */}
+      <div className={styles.TodoBox}>
+        {/* navbar */}
+        <nav className={styles.NavBar}>
+          <CiLight className={styles.LightIcon} />
+
+          <ul className={styles.Menu}>
+            <li className={styles.List}>All</li>
+            <li className={styles.List}>Active</li>
+            <li className={styles.List}>Completed</li>
+          </ul>
+        </nav>
+
+        {/* contents */}
+        <div className={styles.Contents}></div>
+
+        {/* write_input */}
+        <div className={styles.TodoInputWrap}>
+          <input
+            className={styles.InputText}
+            type="text"
+            placeholder="Add Todo..."
+          />
+          <button className={styles.SubmitButton} type="button">
+            Add
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const initialState = [
+  { id: uuidv4(), title: "공부하기", isActive: true },
+  { id: uuidv4(), title: "밥먹기", isActive: true },
+  { id: uuidv4(), title: "강의 보기", isActive: false },
+  { id: uuidv4(), title: "카페가기", isActive: false },
+  { id: uuidv4(), title: "청소하기", isActive: false },
+];
