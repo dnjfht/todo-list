c6067a37b132ad7b7ce7d04b9810fa1da8c70699
Yoo Seungmin <dnjfht@gmail.com>
2023-05-17 03:49:11 +0900

    dark mode - style

--- src/DarkModeContext.js
+++ src/DarkModeContext.js
@@ -37,6 +37,7 @@ function updateDarkMode(darkMode) {
   if (darkMode) {
     document.documentElement.classList.add("dark");
     // 토글링(업데이트)이 될 때마다 로컬 스토리지에도 저장을 해줌.
+    // 로컬 스토리지에 저장을 해주면 새로고침을 해도 상태를 기억하고 있음.
     localStorage.theme = "dark";
   } else {
     document.documentElement.classList.remove("dark");
